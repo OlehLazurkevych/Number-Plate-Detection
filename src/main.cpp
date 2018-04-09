4129b40154cc5132523f5d69344b89e915145b2f
Oleh Lazurkevych <olehlazurkevych@gmail.com>
2018-04-09 12:15:26 +0300

    [New] screnshots

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-	Mat image = imread("E:/Projects/Number-Plate-Recognition/data/photo-of-cars/ez-2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat image = imread("E:/Projects/Number-Plate-Recognition/data/photo-of-cars/ez-1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	
 	if (image.empty())
 	{
@@ -12,15 +12,19 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	Mat* res;
+
 	double t = (double)getTickCount();
 	
 	PlateCropTool cropTool;
 
-	Window::Draw(*cropTool.getPlate(image));
+	res = cropTool.getPlate(image);
 
 	t = ((double)getTickCount() - t) / getTickFrequency();
 	cout << "Done in: " << t << " sec." << endl;
 	
+	Window::Draw(*res);
+
 	waitKey(0);
 	system("pause");
 	return 0;
