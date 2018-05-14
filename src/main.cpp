2f8154c2aa18bcf45a09bc98a1b346c623ffd7f8
Oleh Lazurkevych <olehlazurkevych@gmail.com>
2018-05-14 14:06:12 +0300

    [New] numbers trained

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,103 @@
 ﻿#include "openCV.h"
 #include "numberPlateRecognitionTool.h"
 
+
+
+vector<Point2f> Train(string path, int quantity)
+{
+	CharRecognitionTool test;
+	vector<Point2f> res;
+	char n = '0';
+	char sn = '0' - 1;
+	string postf = ".jpg";
+
+	for (int i = 0; i < quantity; i++)
+	{
+		string dir;
+		if (n == '9' || sn != '0' - 1)
+		{
+			n = '0';
+			dir = path + (++n) + (++sn) + postf;
+		}
+		else
+		{
+			dir = path + (++n) + postf;
+		}
+		Mat image = imread(dir, CV_LOAD_IMAGE_GRAYSCALE);
+		if (!image.empty())
+		{
+			image = *imgLocalThresh(image, 0, 0);
+			vector<Point2f> temp = test.getLEndsVec(image);
+
+			if (res.empty())
+			{
+				res = temp;
+				for (int i = 0; i < res.size(); i++)
+				{
+					cout << res[i].x << "  " << res[i].y << endl;
+				}
+				cout << "======================================================" << endl;
+			}
+			else
+			{
+				list<Point2f> lis;
+
+				for (int j = 0; j < temp.size(); j++)
+				{
+					lis.push_back(temp[j]);
+				}
+
+				list<Point2f>::iterator it = lis.begin();
+				list<Point2f>::iterator itmin = it;
+
+				float min;
+
+				for (int j = 0; j < res.size(); j++)
+				{
+					if (!lis.empty())
+					{
+						it = lis.begin();
+						itmin = it;
+						min = abs(res[j].x - it->x) + abs(res[j].y - it->y);
+
+						for (; it != lis.end(); ++it)
+						{
+							if (abs(res[j].x - it->x) + abs(res[j].y - it->y) < min)
+							{
+								min = abs(res[j].x - it->x) + abs(res[j].y - it->y);
+								itmin = it;
+							}
+						}
+
+						res[j].x += itmin->x;
+						res[j].x /= 2;
+						res[j].y += itmin->y;
+						res[j].y /= 2;
+
+						lis.remove(*itmin);
+					}
+					else
+					{
+						Window::Draw(image);
+						cout << "Train() ! WARNING   :   list is shorter than actual result vector" << endl;
+						break;
+					}
+				}
+			}
+
+			cout << dir << endl;
+		}
+		else
+		{
+			cout << "Train() ! FILE NOT FOUND   :   " << dir << endl;
+		}
+	}
+
+	return res;
+}
+
+
+
 int main(int argc, char** argv)
 {
 	Mat image = imread("E:/Projects/Number-Plate-Recognition/data/training-chars/K/1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
@@ -8,18 +105,18 @@ int main(int argc, char** argv)
 	NumberPlateRecognitionTool NPRT;
 	CharRecognitionTool test;
 
-	Window::Draw(image);
+	//Window::Draw(image);
 
 	
 
-	vector<Point2f> res = test.getLEndsVec(image);
+	vector<Point2f> res = Train("E:/Projects/Number-Plate-Recognition/data/training-chars/9/", 13);
 
 	for (int i = 0; i < res.size(); i++)
 	{
 		cout << res[i].x << "  " << res[i].y << endl;
 	}
 
-	cout << test.getLoopQuantity(image) << endl;
+	//cout << test.getLoopQuantity(image) << endl;
 
 	/*if (image.empty())
 	{
@@ -43,6 +140,3 @@ int main(int argc, char** argv)
 	system("pause");
 	return 0;
 }
-
-
-void 
